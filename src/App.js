0fd13fe90d1bfd61a2f71381a8c62ec477fc9c55
tleung22 <trevor22@uw.edu>
2021-05-05 16:56:41 -0700

    swag

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My reacct app</h1>
+      <h2> aythor trevor</h2>
+    </div>
+  );
+}
+
+export default App;
